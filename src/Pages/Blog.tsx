46ee5740d3c9a60a1b562479830228c276823f12
oluwaseyi-1-Ade <154804947+oluwaseyi-1-Ade@users.noreply.github.com>
2025-01-04 14:27:32 +0100

    Update Blog.tsx

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -1,12 +1,12 @@
 
 const Blog = () => {
     return (
-        <div className="max-w-pageMaxWidth w-full mx-auto mt-10 mb-[700px] rounded-xl lg:mt-[120px] px-5 bg-heroImage bg-cover h-[550px]">
+        <div className="max-w-pageMaxWidth w-full mx-auto mb-[200px] md:mb-[700px] rounded-xl mt-8 lg:mt-[120px] px-5 md:bg-heroImage bg-cover md:h-[550px]">
             {/* <div className="hidden md:block w-full h-[550px] z-10">
                 <img src={heroImage} alt="heroimage" className="w-full h-full" />
             </div> */}
 
-            <div className="max-w-[700px] w-full mx-auto z-50 pt-[400px]">
+            <div className="max-w-[700px] w-full mx-auto z-50 md:pt-[400px]">
                 <form className=" rounded-t-lg p-8 lg:p-16 bg-[#FAFAFC] z-50">
                     <h2 className="font-semibold text-lg text-[#32264D] pb-5 border-b border-b-[#E6E6F0]">Blog Form</h2>
                     <div className="mt-4">
