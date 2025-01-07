361381d219835f9c982122f5bf24b728158123d1
keemah03 <hikmahopeyemi016@gmail.com>
2025-01-07 17:12:39 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Components/Layout";
+import HomePage from "./pages/home";
+import LoginPage from './pages/login';
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: '', element: <HomePage /> },
+      { path: '/login', element: <LoginPage /> }
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes)
 
 const App = () => {
-  return (
-    <Layout />
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+
+function HomePage() {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quidem consequatur aperiam eos expedita voluptatum officiis, dolore error rerum qui asperiores ad aspernatur eum officia in quisquam dolorum, dolorem maxime?</div>
+  )
+}
+
+export default HomePage
