7029262617c26bc1cbe7b4863ad0a04878137c8b
keemah03 <hikmahopeyemi016@gmail.com>
2025-01-07 17:25:32 +0100

    update

--- src/router/Index.tsx
+++ src/router/Index.tsx
@@ -5,21 +5,23 @@ import AuthorPage from "../Pages/AuthorPage";
 import Pages from "../Pages/Pages";
 import SinglePost from "../Pages/SinglePost";
 import Blog from "../Pages/Blog";
+import LoginPage from "../pages/login";
 
 const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="" element={<Layout />}>
-                    <Route path="/" element={<HomePage />}></Route>
-                    <Route path="/blog-form" element={<Blog />}></Route>
-                    <Route path="/single-post" element={<SinglePost />}></Route>
-                    <Route path="/pages" element={<Pages />}></Route>
-                    <Route path="/author-page" element={<AuthorPage />}></Route>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/blog-form" element={<Blog />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/single-post" element={<SinglePost />}></Route>
+          <Route path="/pages" element={<Pages />}></Route>
+          <Route path="/author-page" element={<AuthorPage />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
