66ae36a0f3e468b85ecd24d29ee7113a5b21aa4c
keemah03 <hikmahopeyemi016@gmail.com>
2025-01-12 18:42:17 +0100

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,12 +5,15 @@ function LoginPage() {
       style={{ backgroundImage: "url('/image.png')" }}
     >
       {/*form section*/}
-      <form className="absolute left-1/2 -translate-x-1/2 -bottom-1/4 bg-white w-[42rem] rounded-t-lg p-16">
-        <h3 className="text-xl font-semibold mb-4 text-center md:text-left">
+      <form
+        className="absolute left-1/2 -translate-x-1/2 -bottom-1/4 bg-[#FAFAFC]
+       w-[42rem] rounded-t-lg p-16"
+      >
+        <h3 className="text-xl font-semibold mb-4 text-center md:text-left text-[#32264D]">
           BLOG FORM
         </h3>
         <div className="mb-4">
-          <label htmlFor="title" className="block text-gray-700 font-medium">
+          <label htmlFor="title" className="block text-[#4E4958] font-medium">
             Title
           </label>
           <input
@@ -21,19 +24,20 @@ function LoginPage() {
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="title" className="block text-gray-700 font-medium">
+          <label htmlFor="title" className="block text-[#4E4958] font-medium">
             Description
           </label>
           <textarea
             name="description"
             id="description"
+            rows={4}
             className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
           ></textarea>
         </div>
         <input
           type="submit"
           defaultValue="submit"
-          className="w-full bg-blue-600 text-white font-bold py-3 rounded-lg hover:bg-blue-600"
+          className="w-full bg-[#4B6BFB] text-white font-bold py-3 rounded-lg hover:bg-blue-600"
         />
       </form>
     </div>
